c20d418ec07128a5e395690110d7a57593d75ce6
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-05-25 10:33:46 +0300

    FIR: Report INFERENCE_NO_INFORMATION_FOR_PARAMETER diagnostic

--- compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/inference/model/ConstraintPositionAndErrors.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/inference/model/ConstraintPositionAndErrors.kt
@@ -98,7 +98,7 @@ class CapturedTypeFromSubtyping(
     val position: ConstraintPosition
 ) : ConstraintSystemError(INAPPLICABLE)
 
-abstract class NotEnoughInformationForTypeParameter<T>(
+open class NotEnoughInformationForTypeParameter<T>(
     val typeVariable: TypeVariableMarker,
     val resolvedAtom: T
 ) : ConstraintSystemError(INAPPLICABLE)
