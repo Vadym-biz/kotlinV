apigeoneer/kotlin

Implement spec tests runner for ir interpreter

compiler/tests-spec/tests/org/jetbrains/kotlin/spec/utils/tasks/GenerateSpecTests.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.spec.utils.tasks
 import org.jetbrains.kotlin.generators.impl.generateTestGroupSuite
 import org.jetbrains.kotlin.spec.checkers.AbstractDiagnosticsTestSpec
 import org.jetbrains.kotlin.spec.codegen.AbstractBlackBoxCodegenTestSpec
+import org.jetbrains.kotlin.spec.codegen.AbstractIrInterpreterCodegenTestSpec
 import org.jetbrains.kotlin.spec.parsing.AbstractParsingTestSpec
 import org.jetbrains.kotlin.spec.utils.GeneralConfiguration.SPEC_TESTDATA_PATH
 import org.jetbrains.kotlin.spec.utils.GeneralConfiguration.SPEC_TEST_PATH
@@ -70,6 +71,15 @@ fun generateTests() {
                     "diagnostics",
                     excludeDirs = listOf("helpers") + detectDirsWithTestsMapFileOnly("diagnostics"),
                     excludedPattern = excludedFirTestdataPattern
+                )
+            }
+        }
+
+        testGroup(SPEC_TEST_PATH, SPEC_TESTDATA_PATH) {
+            testClass<AbstractIrInterpreterCodegenTestSpec> {
+                model(
+                    relativeRootPath = "codegen/box",
+                    excludeDirs = listOf("helpers", "templates") + detectDirsWithTestsMapFileOnly("codegen/box"),
                 )
             }
         }

Vadym-biz/kotlinV

compiler/tests-spec/tests/org/jetbrains/kotlin/spec/utils/tasks/GenerateSpecTests.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.spec.utils.tasks
 import org.jetbrains.kotlin.generators.impl.generateTestGroupSuite
 import org.jetbrains.kotlin.spec.checkers.AbstractDiagnosticsTestSpec
 import org.jetbrains.kotlin.spec.codegen.AbstractBlackBoxCodegenTestSpec
+import org.jetbrains.kotlin.spec.codegen.AbstractIrInterpreterBlackBoxCodegenTestSpec
 import org.jetbrains.kotlin.spec.parsing.AbstractParsingTestSpec
 import org.jetbrains.kotlin.spec.utils.GeneralConfiguration.SPEC_TESTDATA_PATH
 import org.jetbrains.kotlin.spec.utils.GeneralConfiguration.SPEC_TEST_PATH
@@ -70,6 +71,15 @@ fun generateTests() {
                     "diagnostics",
                     excludeDirs = listOf("helpers") + detectDirsWithTestsMapFileOnly("diagnostics"),
                     excludedPattern = excludedFirTestdataPattern
+                )
+            }
+        }
+
+        testGroup(SPEC_TEST_PATH, SPEC_TESTDATA_PATH) {
+            testClass<AbstractIrInterpreterBlackBoxCodegenTestSpec> {
+                model(
+                    relativeRootPath = "codegen/box",
+                    excludeDirs = listOf("helpers", "templates") + detectDirsWithTestsMapFileOnly("codegen/box"),
                 )
             }
         }