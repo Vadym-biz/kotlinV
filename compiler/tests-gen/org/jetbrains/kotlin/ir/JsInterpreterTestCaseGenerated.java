swimmikerun/kotlin

Create some tests for property reference interpretation

compiler/tests/org/jetbrains/kotlin/ir/JsInterpreterTestCaseGenerated.java
@@ -539,4 +539,22 @@ public class JsInterpreterTestCaseGenerated extends AbstractJsInterpreterTestCas
             runTest("compiler/testData/ir/interpreter/proxy/superWrapper.kt");
         }
     }
+
+    @TestMetadata("compiler/testData/ir/interpreter/reference")
+    @TestDataPath("$PROJECT_ROOT")
+    @RunWith(JUnit3RunnerWithInners.class)
+    public static class Reference extends AbstractJsInterpreterTestCase {
+        private void runTest(String testDataFilePath) throws Exception {
+            KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
+        }
+
+        public void testAllFilesPresentInReference() throws Exception {
+            KotlinTestUtils.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/ir/interpreter/reference"), Pattern.compile("^(.+)\\.kt(s)?$"), null, true);
+        }
+
+        @TestMetadata("propertyReference.kt")
+        public void testPropertyReference() throws Exception {
+            runTest("compiler/testData/ir/interpreter/reference/propertyReference.kt");
+        }
+    }
 }

compiler/tests/org/jetbrains/kotlin/ir/JvmInterpreterTestCaseGenerated.java
@@ -557,4 +557,22 @@ public class JvmInterpreterTestCaseGenerated extends AbstractJvmInterpreterTestC
             runTest("compiler/testData/ir/interpreter/proxy/superWrapper.kt");
         }
     }
+
+    @TestMetadata("compiler/testData/ir/interpreter/reference")
+    @TestDataPath("$PROJECT_ROOT")
+    @RunWith(JUnit3RunnerWithInners.class)
+    public static class Reference extends AbstractJvmInterpreterTestCase {
+        private void runTest(String testDataFilePath) throws Exception {
+            KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
+        }
+
+        public void testAllFilesPresentInReference() throws Exception {
+            KotlinTestUtils.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/ir/interpreter/reference"), Pattern.compile("^(.+)\\.kt(s)?$"), null, true);
+        }
+
+        @TestMetadata("propertyReference.kt")
+        public void testPropertyReference() throws Exception {
+            runTest("compiler/testData/ir/interpreter/reference/propertyReference.kt");
+        }
+    }
 }

Vadym-biz/kotlinV

compiler/tests-gen/org/jetbrains/kotlin/ir/JsInterpreterTestCaseGenerated.java
@@ -540,4 +540,22 @@ public class JsInterpreterTestCaseGenerated extends AbstractJsInterpreterTestCas
             runTest("compiler/testData/ir/interpreter/proxy/superWrapper.kt");
         }
     }
+
+    @TestMetadata("compiler/testData/ir/interpreter/reference")
+    @TestDataPath("$PROJECT_ROOT")
+    @RunWith(JUnit3RunnerWithInners.class)
+    public static class Reference extends AbstractJsInterpreterTestCase {
+        private void runTest(String testDataFilePath) throws Exception {
+            KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
+        }
+
+        public void testAllFilesPresentInReference() throws Exception {
+            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/ir/interpreter/reference"), Pattern.compile("^(.+)\\.kt(s)?$"), null, true);
+        }
+
+        @TestMetadata("propertyReference.kt")
+        public void testPropertyReference() throws Exception {
+            runTest("compiler/testData/ir/interpreter/reference/propertyReference.kt");
+        }
+    }
 }

compiler/tests-gen/org/jetbrains/kotlin/ir/JvmInterpreterTestCaseGenerated.java
@@ -558,4 +558,22 @@ public class JvmInterpreterTestCaseGenerated extends AbstractJvmInterpreterTestC
             runTest("compiler/testData/ir/interpreter/proxy/superWrapper.kt");
         }
     }
+
+    @TestMetadata("compiler/testData/ir/interpreter/reference")
+    @TestDataPath("$PROJECT_ROOT")
+    @RunWith(JUnit3RunnerWithInners.class)
+    public static class Reference extends AbstractJvmInterpreterTestCase {
+        private void runTest(String testDataFilePath) throws Exception {
+            KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
+        }
+
+        public void testAllFilesPresentInReference() throws Exception {
+            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/ir/interpreter/reference"), Pattern.compile("^(.+)\\.kt(s)?$"), null, true);
+        }
+
+        @TestMetadata("propertyReference.kt")
+        public void testPropertyReference() throws Exception {
+            runTest("compiler/testData/ir/interpreter/reference/propertyReference.kt");
+        }
+    }
 }